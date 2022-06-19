9df688f752316ca08f99bcebdf0af3723dcfcc38
Snopphin <81505000+Snopphin@users.noreply.github.com>
2022-06-19 14:47:44 +0300

    Update Render.h

--- ABot Non Live/Render.h
+++ ABot Non Live/Render.h
@@ -5,7 +5,7 @@ class Render
 public:
 	explicit Render(fast_vector<FrameAction>& Actions, float Fps, std::string ClickPack, float Volume);
 private:
-	void OnAction(FrameAction& Action, float Fps, const std::string& ClickPack, float Volume);
+	void OnAction(FrameAction Action, float Fps, const std::string& ClickPack, float Volume);
 private:
 	AudioEngine m_Output;
 };
